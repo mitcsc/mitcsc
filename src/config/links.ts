bf01b576baf6c75d23aa0b04fb6e32d7cb62fb6e
Stanley <mitcsc@gmail.com>
2025-09-09 10:22:17 -0400

    Update links.ts

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,5 +1,4 @@
-export const membership =
-  "https://docs.google.com/forms/d/e/1FAIpQLScuVLsA4Zi620hS5fDlHlT4VBvrSEiA5LTCdD5yNHbnArTGnQ/viewform";
+export const membership = "https://forms.gle/QG77uzXDoPBz6MWG8";
 
 const calendarLink =
   "https://calendar.google.com/calendar/u/0?cid=OWEzaDZkOGhsam5ybmF1NzhqZDJlcWk5ZXNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ";
